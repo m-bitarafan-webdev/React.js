33701559b44eac80174655d92db4a56e17259f30
Mahdi Bitarafan <m.bitarafan.webdev@gmail.com>
2024-10-27 00:16:05 +0330

    Minor changes, input bug

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -5,22 +5,38 @@ const App = () => {
   //App component was made and the return statement clarified
   const [todos, setTodos] = useState([]);
   //a state of todo list was set and a function to update it was defined by the empty array in useState hook
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //Logic to add a new todo
+    const newTodo = { id: Date.now(), text:  input ,completed: false };
+    //an object of todo instance was made to include the properties related
+    setTodos([...todos, newTodo])
+  }
   return (
     <div>
       <h1>Todo App</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
-        type="text"
-        placeholder="What to-do next?"
-        required
+          type="text"
+          placeholder="What to-do next?"
+          required
         />
+        <button type="submit">Add</button>
       </form>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </div>
     
   )
   //a single div contating an h1 header included
   //a form element with the type and placeholder and a condition to fill-in was created
+  //added a submit button
 }
 
 export default App;
 //I also exported the App component
+//Nice changes have made and now I wanna push it to Github
+//Second commit: this time I don't know how to connect the input section value to that of the todo object
